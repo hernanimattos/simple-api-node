d3903840c44b4dbccfd69f31a44cb797e7ee60dc
hernani <hernani.luiz@gmail.com>
2018-04-19 23:53:14 -0300

    Connect mongo

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var routes = require('./routes/index')
-var port = process.env.PORT || 3100;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/api-test");
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function(res) {
+  // we're connected!
+
+  console.log('ff', res)
+});
+
+const port = process.env.PORT || 3100;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
