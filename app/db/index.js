5773070e64e79e755852c7b503cee4f01e617a53
hernani <hernani.luiz@gmail.com>
2018-07-26 00:37:58 -0300

    Pass node simple api to ES6 with babel

--- app/db/index.js
+++ app/db/index.js
@@ -1,38 +1,40 @@
-
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 mongoose.connect("mongodb://localhost:27017/api-study")
 const db = mongoose.connection;
-let connect = function() {
-		db.on("error", (error) => {
+const connect = () => {
+	db.on("error", (error) => {
 
-			console.error.bind(console, "connection error:")
+		console.error.bind(console, "connection error:")
 
-		});
-		db.once("open", (res) => {
+	});
+	db.once("open", (res) => {	
 
-			console.log('Mongo connection is open', res)
+		console.log('Mongo connection is open', res)
 
-		});
-		db.on("connected", (res) => {
+	});
+	db.on("connected", (res) => {
 
-			console.log("Mongo connected", res)
-			return true;
+		console.log("Mongo connected", res)
+		return true;
 
-		});
-		db.on("disconnected", (ref) => {
+	});
+	db.on("disconnected", (ref) => {
 
-			console.log(" Mongo disconnected", ref)
+		console.log(" Mongo disconnected", ref)
 
-		});
+	});
 };
 
-let stop = () => {
+const stop = () => {
 	db.close(() => {
 		process.exit(0);
 	})
 }
+export {
+	stop,
+	connect
+}
 
-module.exports = stop;
-module.exports = connect;
+
 
 
--- app/routes/task.js
+++ app/routes/task.js
@@ -1,11 +1,11 @@
-const express = require('express');
+import express from "express";
 const router = express.Router();
-const taskController = require('../controllers/task.js');
+import taskController from '../controllers/task.js';
 
 router.get('/tasks', taskController.alltasks)
 router.get('/task/:id', taskController.findById)
 router.post('/task', taskController.create)
 router.put('/task/:id', taskController.edit)
-router.delete('/task/:id', taskController.delete)
+router.delete('/task/:id', taskController._delete)
 
 module.exports = router;
